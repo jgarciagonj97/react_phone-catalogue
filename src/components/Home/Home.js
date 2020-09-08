a80dd161ccaadd8e16564f2ff42e16dbfe9c3eb9
jgarciagonj97 <j.garciagonj@gmail.com>
2020-09-08 13:54:41 +0200

    Creating Dockerfile.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,13 @@
 import React from "react";
 import "./Home.scss";
-import { useHistory } from "react-router-dom";
 
 const HomePage = () => {
-    const history = useHistory();
-    const handleClick = () => {
-        history.push("/phones");
-    };
     return (
         <div>
             <div className="contenedor_home">
             </div>
             <div className="titulo">
-                <h1> WELCOME TO THE FIRST PHONE CATALOGUE DEVELOPED BY GUIDESMITHS </h1 >
+                <h1> PHONE CATALOGUE CREATED BY @JGARCIAGONJ97 </h1 >
             </div>
         </div>
     );
